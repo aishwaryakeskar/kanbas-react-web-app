4e7d6491fd2ad9f044d5573d22d3955d7e465750
aishwaryakeskar <aishwaryakeskar98@gmail.com>
2024-02-17 18:58:24 -0500

    Add Kanbas Navigation

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,10 +1,18 @@
 import Nav from "../Nav";
+import KanbasNavigation from "./KanbasNavigation";
+
 function Kanbas() {
- return (
-   <div>
-     <Nav/>
-     <h1>Kanbas</h1>
-   </div>
- )
+  return (
+    <div className="d-flex">
+      <div>
+        <KanbasNavigation/>
+      </div>
+      <div style={{ flexGrow: 1 }}>
+        <h1>Account</h1>
+        <h1>Dashboard</h1>
+        <h1>Courses</h1>
+      </div>
+    </div>
+  );
 }
-export default Kanbas
+export default Kanbas;
