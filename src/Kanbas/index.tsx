401d5879e000d319c304dcaa9a663769d70ebb3c
aishwaryakeskar <aishwaryakeskar98@gmail.com>
2024-02-17 21:39:55 -0500

    Add Dashboard

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -1,5 +1,6 @@
-import Nav from "../Nav";
 import KanbasNavigation from "./KanbasNavigation";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Dashboard from "./Dashboard";
 
 function Kanbas() {
   return (
@@ -8,9 +9,12 @@ function Kanbas() {
         <KanbasNavigation/>
       </div>
       <div style={{ flexGrow: 1 }}>
-        <h1>Account</h1>
-        <h1>Dashboard</h1>
-        <h1>Courses</h1>
+      <Routes>
+          <Route path="/" element={<Navigate to="Dashboard" />} />
+          <Route path="Account" element={<h1>Account</h1>} />
+          <Route path="Dashboard" element={<Dashboard />} />
+          <Route path="Courses/*" element={<h1>Courses</h1>} />
+        </Routes>
       </div>
     </div>
   );
